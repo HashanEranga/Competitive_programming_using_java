82805f4238fa35c94414f17c0c3e09eb0dbcab08
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-22 20:46:48 +0530

    day 2 | java fundamentals

---
+++ day02_loops/src/ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        // without using iterations
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello world");
+        }
+    }
+}
+
+//create for loops for iterations
+//ctrl + shift + slash for commenting multiple lines
